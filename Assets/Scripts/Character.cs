0d90abb603b43f69e58c719009a8d6ef5f091cd4
neviovalsa <nevio.valsa@gmail.com>
2023-05-28 16:04:51 -0700

    feat: add circle under character

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Team
+{
+    Radiant,
+    Dire
+}
 public class Character : MonoBehaviour, ITargetable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Team team;
+    
+    public Team Team { get { return team; } }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private ParticleSystem _clickParticle;
     [SerializeField] private HeroAI _heroAI;
+    [SerializeField] private RectTransform _rectTransform;
 
     private NavMeshAgent _navMeshAgent;
-
     private Character _characterSelf => _heroAI.GetComponent<Character>();
+
+    public bool LeftClickDown { get; private set; }
+
+    Vector3 StartMousePosition;
     private void Awake()
     {
         _navMeshAgent = _heroAI.GetComponent<NavMeshAgent>();
@@ -22,6 +26,31 @@ public class PlayerController : MonoBehaviour
         {
             OnRightClick();
         }
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            _rectTransform.gameObject.SetActive(true);
+            StartMousePosition = Input.mousePosition;
+            LeftClickDown = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            _rectTransform.gameObject.SetActive(false);
+            LeftClickDown = false;
+        }
+
+        if(LeftClickDown)
+        {
+            ResizeSelecionBox();
+        }
+    }
+    
+    private void ResizeSelecionBox()
+    {
+        float width = Input.mousePosition.x - StartMousePosition.x;
+        float height = Input.mousePosition.y - StartMousePosition.y;
+
+        _rectTransform.anchoredPosition = new Vector2(StartMousePosition.x, StartMousePosition.y ) + new Vector2(width / 2, height / 2);
+        _rectTransform.sizeDelta = new Vector2(Mathf.Abs(width), Mathf.Abs(height));
     }
 
     private void OnRightClick()
